f0f42a3d26bf4c1cb1f7f19f1b2f42ff9c3ed16e
ArtemStar2 <artemdegtyrov@gmail.com>
2023-05-13 23:45:44 +0300

    1

--- api/app.js
+++ api/app.js
@@ -25,7 +25,7 @@ app.use(cookieParser())
 app.use(fileUpload())
 app.use(cors({
     credentials: true,
-    origin: process.env.CLIENT_URL
+    origin: 'https://service-restaurant-admin-panel.vercel.app/'
 }))
 // app.use('/uploads', express.static(path.join(__dirname,'../uploads')))
 app.use(express.static(path.join(__dirname, 'public')));
