cc35679f9933b53321cdb6c95bff2666e7fb3c28
ArtemStar2 <artemdegtyrov@gmail.com>
2023-05-14 01:55:33 +0300

    Update app.js

--- api/app.js
+++ api/app.js
@@ -22,6 +22,13 @@ const orderRoutes = require('./orders/orders.router')
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //     next();
 // });
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+});
 app.use(cors({origin: process.env.CLIENT_URL}));
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
