3b0d05458ecbdbf1c887042c3bfe120e63c440a5
ArtemStar2 <artemdegtyrov@gmail.com>
2023-05-14 02:54:20 +0300

    Update app.js

--- api/app.js
+++ api/app.js
@@ -17,11 +17,29 @@ const orderRoutes = require('./orders/orders.router')
 // /Routers
 
 // Config
-app.use(cors({
-    credentials: true,
-    origin: process.env.CLIENT_URL,
-    methods: "GET,PUT,POST,DELETE",
-}));
+const allowlist = [process.env.CLIENT_URL, 'http://localhost:5173'];
+
+const corsOptionsDelegate = (req, callback) => {
+    let corsOptions;
+
+    let isDomainAllowed = whitelist.indexOf(req.header('Origin')) !== -1;
+    let isExtensionAllowed = req.path.endsWith('.jpg');
+
+    if (isDomainAllowed && isExtensionAllowed) {
+        // Enable CORS for this request
+        corsOptions = { origin: true }
+    } else {
+        // Disable CORS for this request
+        corsOptions = { origin: false }
+    }
+    callback(null, corsOptions)
+}
+app.use(cors(corsOptionsDelegate));
+// app.use(cors({
+//     credentials: true,
+//     origin: process.env.CLIENT_URL,
+//     methods: "GET,PUT,POST,DELETE",
+// }));
 // app.use(cors({origin: process.env.CLIENT_URL}));
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
