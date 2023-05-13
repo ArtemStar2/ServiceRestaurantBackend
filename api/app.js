d93bcaaf7587ceb7517cb188ba25c43f47b14ad7
ArtemStar2 <artemdegtyrov@gmail.com>
2023-05-14 02:46:44 +0300

    Update app.js

--- api/app.js
+++ api/app.js
@@ -18,13 +18,11 @@ const orderRoutes = require('./orders/orders.router')
 
 // Config
 
-app.use(function (req, res, next) {
-    //Enabling CORS
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Max-Age", "1800");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,token");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Content-Type", "application/json;charset=utf-8");
     next();
 });
 app.use(cors({
