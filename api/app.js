5cdeede9e71cb1ef644032011394fa925da3db7d
ArtemStar2 <artemdegtyrov@gmail.com>
2023-05-14 00:46:09 +0300

    Update app.js

--- api/app.js
+++ api/app.js
@@ -22,10 +22,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser())
 app.use(fileUpload())
-app.use(cors({
-    // credentials: true,
-    origin: process.env.CLIENT_URL
-}))
+app.use(cors())
 // app.use('/uploads', express.static(path.join(__dirname,'../uploads')))
 app.use(express.static(path.join(__dirname, 'public')));
 // /Config
