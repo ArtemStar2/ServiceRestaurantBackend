c52c69f4e75f2eb600fd3176e1d08552a2ce116d
ArtemStar2 <artemdegtyrov@gmail.com>
2023-05-14 02:11:27 +0300

    7

--- api/app.js
+++ api/app.js
@@ -17,19 +17,7 @@ const orderRoutes = require('./orders/orders.router')
 // /Routers
 
 // Config
-// app.use(function (req, res, next) {
-//     //Enabling CORS
-//     res.setHeader('Access-Control-Allow-Credentials', true)
-//     res.setHeader('Access-Control-Allow-Origin', '*')
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-//     res.setHeader(
-//         'Access-Control-Allow-Headers',
-//         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-//       )
-//     next();
-// });
-app.use(cors({credentials: true,origin: 'http://localhost:5173'}));
+app.use(cors({credentials: true,origin: process.env.CLIENT_URL}));
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser())
