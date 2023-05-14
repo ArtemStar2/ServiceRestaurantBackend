01331c99a76d030d524bbaf6e74c7fa36763c3f0
ArtemStar2 <artemdegtyrov@gmail.com>
2023-05-14 04:37:19 +0300

    Update products.controller.js

--- api/products/products.controller.js
+++ api/products/products.controller.js
@@ -29,8 +29,9 @@ class productsControllers{
     
     async createProduct(req, res, next){
         try{
-            const { name, description, price, category } = req.body
-            const userData = await productService.createProduct(name, description, req.files?.images, price, category, req.user.role)
+            const { name, description, price, category, images } = req.body
+            // req.files?.images
+            const userData = await productService.createProduct(name, description, images, price, category, req.user.role)
             return res.status(200).json(userData);
         } catch(e){
             next(e)
