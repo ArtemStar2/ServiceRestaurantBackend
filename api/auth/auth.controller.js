5ea62a50d8f8a9f7563bb663f41eb0b820e36f28
ArtemStar2 <artemdegtyrov@gmail.com>
2023-05-15 08:04:19 +0300

    34

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -4,6 +4,7 @@ class authControllers{
     async auth(req, res, next){
         try{
             const { login } = req.body
+            console.log('login' + login);
             const userData = await authService.auth(login)
             res.cookie('refreshToken', userData.refreshToken, { maxAge: 30*24*60*60*1000, httpOnly: true})
             return res.status(200).json(userData);
