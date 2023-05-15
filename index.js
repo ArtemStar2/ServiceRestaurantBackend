ab533446000f6ec47974d2d3bd3558bae7e07b62
ArtemStar2 <artemdegtyrov@gmail.com>
2023-05-15 09:22:07 +0300

    2

--- index.js
+++ index.js
@@ -10,55 +10,6 @@ const bot = new TelegramApi(token, {polling: true})
 const chat = {}
 const webAppUrl = 'https://service-restaurant-admin-panel.vercel.app/';
 
-bot.setMyCommands([
-    {command: "/start", description: "Запустить бота"},
-    {command: "/menu", description: "Меню"},
-    {command: "/events", description: "Мероприятие"},
-    {command: "/waiter", description: "Позвать официанта"},
-    {command: "/table", description: "Забронировать стол"},
-    {command: "/contact", description: "Наши контакты"},
-    {command: "/profile", description: "Профиль"}
-])
-
-
-bot.on('callback_query', msg =>{
-    const data = msg.data;
-    const chatId = msg.message.chat.id;
-    if(data == 'menu'){
-        console.log('menu')
-    }
-    console.log(msg);
-})
-
-bot.on('message', async msg => {
-    const text = msg.text;
-    const chatId = msg.chat.id;
-
-    if(text == '/start'){
-        return bot.sendMessage(chatId, `Вы запустили ресторан бота`, {
-            reply_markup: {
-                inline_keyboard: [
-                    [{text: 'Меню', web_app:{url: webAppUrl + 'menu/'}}],
-                    [{text: 'Мероприятие', callback_data: 'events'}],
-                    [{text: 'Позвать официанта', callback_data: 'waiter'}],
-                    [{text: 'Забронировать стол', callback_data: 'table'}],
-                    [{text: 'Наши контакты', callback_data: 'contact'}],
-                    [{text: 'Профиль', callback_data: 'profile'}],
-                ]
-            }
-        })
-    }
-    if(text == "/menu"){
-        return bot.sendMessage(chatId, `Меню`, {
-            reply_markup: {
-                inline_keyboard: [
-                    [{text: 'Меню', web_app:{url: webAppUrl}}],
-                ]
-            }
-        })
-    }
-    return bot.sendMessage(chatId, `Неверная команда`)
-})
 
 
 const start = async () => {
