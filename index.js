b84549d149a081b88bc71d39f25925146868c12d
ArtemStar2 <artemdegtyrov@gmail.com>
2023-05-15 09:27:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
-
+const TelegramApi = require('node-telegram-bot-api')
 const app = require('./api/app');
 const PORT = process.env.PORT || 8080
 
-const TelegramApi = require('node-telegram-bot-api')
+
 const token = '6190170133:AAEumUhRkTDn0MJ7hN4F8U2BPxemn1Q4WQI';
 const bot = new TelegramApi(token, {polling: true})
 
@@ -21,44 +21,55 @@ bot.setMyCommands([
 ])
 
 
-bot.on('callback_query', msg =>{
-    const data = msg.data;
-    const chatId = msg.message.chat.id;
-    if(data == 'menu'){
-        console.log('menu')
-    }
-    console.log(msg);
-})
+const startBot = () => {
+    bot.on('callback_query', msg =>{
+        const data = msg.data;
+        const chatId = msg.message.chat.id;
+        if(data == 'menu'){
+            console.log('menu')
+        }
+        console.log(msg);
+    })
 
-bot.on('message', async msg => {
-    const text = msg.text;
-    const chatId = msg.chat.id;
+    bot.on('message', async msg => {
+        const text = msg.text;
+        const chatId = msg.chat.id;
+    
+        if(text == '/start'){
+            return bot.sendMessage(chatId, `Вы запустили ресторан бота`, {
+                reply_markup: {
+                    inline_keyboard: [
+                        [{text: 'Меню', web_app:{url: webAppUrl + 'menu/'}}],
+                        [{text: 'Мероприятие', callback_data: 'events'}],
+                        [{text: 'Позвать официанта', callback_data: 'waiter'}],
+                        [{text: 'Забронировать стол', callback_data: 'table'}],
+                        [{text: 'Наши контакты', callback_data: 'contact'}],
+                        [{text: 'Профиль', callback_data: 'profile'}],
+                    ]
+                }
+            })
+        }
+        if(text == "/menu"){
+            return bot.sendMessage(chatId, `Меню`, {
+                reply_markup: {
+                    inline_keyboard: [
+                        [{text: 'Меню', web_app:{url: webAppUrl}}],
+                    ]
+                }
+            })
+        }
+        if(text == "/events"){
+            return bot.sendMessage(chatId, `Меню`)
+        }
+        if(text == "/waiter"){
+            return bot.sendMessage(chatId, `Напишите номер стола:`)
 
-    if(text == '/start'){
-        return bot.sendMessage(chatId, `Вы запустили ресторан бота`, {
-            reply_markup: {
-                inline_keyboard: [
-                    [{text: 'Меню', web_app:{url: webAppUrl + 'menu/'}}],
-                    [{text: 'Мероприятие', callback_data: 'events'}],
-                    [{text: 'Позвать официанта', callback_data: 'waiter'}],
-                    [{text: 'Забронировать стол', callback_data: 'table'}],
-                    [{text: 'Наши контакты', callback_data: 'contact'}],
-                    [{text: 'Профиль', callback_data: 'profile'}],
-                ]
-            }
-        })
-    }
-    if(text == "/menu"){
-        return bot.sendMessage(chatId, `Меню`, {
-            reply_markup: {
-                inline_keyboard: [
-                    [{text: 'Меню', web_app:{url: webAppUrl}}],
-                ]
-            }
-        })
-    }
-    return bot.sendMessage(chatId, `Неверная команда`)
-})
+        }
+        return bot.sendMessage(chatId, `Неверная команда`)
+    })
+}
+
+startBot()
 
 
 const start = async () => {
